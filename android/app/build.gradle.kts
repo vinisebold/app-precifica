d51a7d642b4db6f5d8a22f7ed1e21a8888ee10fb
Vinicius Sebold <vinicius.sebold05@gmail.com>
2026-01-07 16:10:13 -0300

    fix(build.gradle.kts): update namespace to dev.vinisebold.precifica

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.precifica"
+    namespace = "dev.vinisebold.precifica"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
